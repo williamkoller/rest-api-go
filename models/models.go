f9756ddad21ec254ea40ce6df672e8d04588d41b
William Koller <williamkoller30@gmail.com>
2023-05-23 02:12:28 -0300

    refactor: alter model Fact

--- models/models.go
+++ models/models.go
@@ -6,6 +6,6 @@ type Fact struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Question  string    `json:"question" gorm:"text;not null;default:null`
-	Answer    string    `json:"answer" gorm:"text;not null;default:null`
+	Question  string    `json:"question" gorm:"text"`
+	Answer    string    `json:"answer" gorm:"text"`
 }
